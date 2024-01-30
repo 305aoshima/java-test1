91444f9e10061585dd7ac1db29f7e1accec393ec
y2.aoshima <77441154+305aoshima@users.noreply.github.com>
2024-01-30 12:04:34 +0900

    Update HelloTest.java

--- test/HelloTest.java
+++ test/HelloTest.java
@@ -3,7 +3,7 @@ import org.testng.annotations.*;
 
 public class HelloTest {
   @Test public void test001() { D("test001"); }
-  static void debu(String format, Object... args) {
+  static void D(String format, Object... args) {
     System.out.printf(format + "\n", args);
   }
 }
